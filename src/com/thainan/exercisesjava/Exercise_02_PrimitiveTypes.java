bb2ea181c7de31368a64f6a2d7060f23864c876c
Thainan Sousa <thainansousa7@gmail.com>
2023-11-19 20:58:52 -0300

    other primitive types

--- src/com/thainan/exercisesjava/Exercise_02_PrimitiveTypes.java
+++ src/com/thainan/exercisesjava/Exercise_02_PrimitiveTypes.java
@@ -15,6 +15,7 @@ public class Exercise_02_PrimitiveTypes {
        // Create variable typeof int
        
        int yearOfTheCreationJavaLanguage; // Variable create in the memory space
+      
        
        yearOfTheCreationJavaLanguage = 1995; // Variable initializable
        
@@ -27,5 +28,17 @@ public class Exercise_02_PrimitiveTypes {
         
         System.out.println("The Java programming language was released in the year " + yearOfTheCreationJavaLanguage);
         
+        
+        // Other types
+        
+        long bigNumber; // 8 bytes
+        double salary; // 8 bytes
+        float height; // 4 bytes
+        byte space; // 1 byte / -128 to 127
+        short smallNumber; // 2 bytes
+        boolean isOpen; // 1 bit - 0 or 1 and True or False
+        char character; // Ex: 'A'
+        
+        // int 4 bytes
     }
 }
