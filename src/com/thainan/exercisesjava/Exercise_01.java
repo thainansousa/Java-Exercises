0e525e466547c57b960348957fdf3392003a71b4
Thainan Sousa <thainansousa7@gmail.com>
2023-11-19 20:20:55 -0300

    Exercise 01 - Hello word Java!

---
+++ src/com/thainan/exercisesjava/Exercise_01.java
@@ -0,0 +1,11 @@
+package com.thainan.exercisesjava;
+
+/**
+ *
+ * @author thainan
+ */
+public class Exercise_01 {
+    public static void main(String[] args){
+        System.out.println("Olá Mundo Java is the better!!!");
+    }
+}
