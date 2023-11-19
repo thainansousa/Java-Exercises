aaaea3cf3e03d0fc9f7269699ee5fe68ade8ae17
Thainan Sousa <thainansousa7@gmail.com>
2023-11-19 20:45:59 -0300

    int primitive type

---
+++ src/com/thainan/exercisesjava/Exercise_02_PrimitiveTypes.java
@@ -0,0 +1,31 @@
+package com.thainan.exercisesjava;
+
+/**
+ *
+ * @author thainan
+ */
+public class Exercise_02_PrimitiveTypes {
+    public static void main(String[] args){
+        /* The types are:
+        
+            int, double, float, char, byte, short, long, boolean 
+        
+        */
+        
+       // Create variable typeof int
+       
+       int yearOfTheCreationJavaLanguage; // Variable create in the memory space
+       
+       yearOfTheCreationJavaLanguage = 1995; // Variable initializable
+       
+        System.out.println(yearOfTheCreationJavaLanguage); // Print result of the variable in the console
+        
+        
+        // Concatenate year with phrase
+        
+        // Use text plus signal (+) for the concatenate
+        
+        System.out.println("The Java programming language was released in the year " + yearOfTheCreationJavaLanguage);
+        
+    }
+}
