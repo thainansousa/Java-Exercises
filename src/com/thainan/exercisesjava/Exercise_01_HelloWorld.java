458e9b97158775932a6e5220eef1dddf92d6ef3a
Thainan Sousa <thainansousa7@gmail.com>
2023-11-19 20:26:30 -0300

    rename exercise 01

--- src/com/thainan/exercisesjava/Exercise_01_HelloWorld.java
+++ src/com/thainan/exercisesjava/Exercise_01_HelloWorld.java
@@ -4,8 +4,8 @@ package com.thainan.exercisesjava;
  *
  * @author thainan
  */
-public class Exercise_01 {
+public class Exercise_01_HelloWorld {
     public static void main(String[] args){
         System.out.println("Olá Mundo Java is the better!!!");
     }
-}
+}g
